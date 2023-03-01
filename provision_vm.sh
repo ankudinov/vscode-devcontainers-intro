af2a59149f7bd23a23faf8c30b6418244b1c05e7
ankudinov <petr.ankudinov@gmail.com>
2023-03-01 18:06:35 +0000

    fix vagrant prov script

--- provision_vm.sh
+++ provision_vm.sh
@@ -5,4 +5,4 @@ set +e
 curl -fsSL https://get.docker.com | sh
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
 # add user to the docker group
-sudo usermod -aG docker $USER
+sudo usermod -aG docker vagrant
